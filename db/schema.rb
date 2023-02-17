90ef48e31c4019037ac5c5d16e0a54a3297f6dd9
DiegoCoronaBarragan <d.coronabarragan@gmail.com>
2023-02-16 18:17:42 -0600

    Add cupon model
    We edit the migration with all the columms pertinents.
    
    Co-authored-by: EverardoAlcarazBaltazar <ealcaraz12@ucol.mx>
    Co-authored-by: oscaregr <oegr.el.may@gmail.com>
    Co-authored-by: Ewar22 <montanoeduards321@gmail.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,29 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_16_220942) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_16_234420) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cupons", force: :cascade do |t|
+    t.bigint "user_id"
+    t.string "url"
+    t.string "title"
+    t.string "description"
+    t.string "location"
+    t.string "image_url"
+    t.string "normal_price"
+    t.string "discount_price"
+    t.string "discount_percentage"
+    t.string "coupon"
+    t.integer "punctuation"
+    t.string "type"
+    t.datetime "start_date"
+    t.datetime "expiration_date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_cupons_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -29,4 +49,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_16_220942) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "cupons", "users"
 end
