84a36d779d025d13e498f12e6fe6bb4939709359
eduard issael montaño arreola <117693797+Ewar22@users.noreply.github.com>
2023-03-15 17:00:56 -0600

    Create Crud Category
    We Create the Crud Category
    
    Co-authored-by: DiegoCoronaBarragan <d.coronabarragan@gmail.com>

---
+++ app/views/categories/_category.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! category, :id, :namecategory, :created_at, :updated_at
+json.url category_url(category, format: :json)
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_02_020015) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_15_224711) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "categories", force: :cascade do |t|
-    t.string "name"
+    t.string "namecategory"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -42,8 +42,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_02_020015) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
     t.string "name"
+    t.string "encrypted_password", default: "", null: false
     t.string "user_type"
     t.string "follower"
     t.string "reset_password_token"
