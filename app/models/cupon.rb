fc90d993767a210f6424a170df623db5d460cdea
oscaregr <bavuey@hotmail.com>
2023-03-17 20:47:06 -0600

    Add restrictions to inputs in app
    The user name and cupon title now have a restriction of characters allowed.
    User name only can has 20 characters and not be less to 3 characters.
    User name and Cupon title are convert to downcase before save them.

--- app/models/cupon.rb
+++ app/models/cupon.rb
@@ -1,3 +1,15 @@
 class Cupon < ApplicationRecord
   belongs_to :user
+
+  validates :title, presence: true, 
+  format: {
+    with: /\A[a-z0-9A-Z\s]+\z/
+  }
+
+  before_save :downcase_atributes
+  private
+
+    def downcase_atributes
+      self.title = title.downcase
+    end
 end
