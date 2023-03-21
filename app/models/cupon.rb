e280783c707f07de01773fbf90f7e9ec53d224c2
ever <ealcaraz12@ucol.mx>
2023-03-21 15:06:43 -0600

    map view added

--- app/models/cupon.rb
+++ app/models/cupon.rb
@@ -1,3 +1,14 @@
 class Cupon < ApplicationRecord
   belongs_to :user
+
+  validates :location, presence: true,
+    format: {
+      with: /\A[a-z0-9A-ZÁáÉéÍíÓóÚú,. ]+\z/
+    }
+
+  before_save :downcase_atributes
+  private
+  def downcase_atributes
+    self.location = location.downcase
+  end
 end
