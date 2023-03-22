1199b86e0388cfb6e150feafeb1795a54aeb7ca6
oscaregr <bavuey@hotmail.com>
2023-03-21 18:50:39 -0600

    Search for user and cupon.
    The header search bar can find users or coupons by name and title.
    The search bar gets data when the input has more than one character.
    There is a default response when no data is obtained.

--- app/controllers/mainpages_controller.rb
+++ app/controllers/mainpages_controller.rb
@@ -1,5 +1,13 @@
 class MainpagesController < ApplicationController
-    def index; 
-        @cupon = Cupon.all
+  def index
+    @result = params[:search].nil? ? Cupon.all : (params[:search].length > 1 ? find : Cupon.all)
+  end
+
+  private
+
+    def find
+      result = User.select(:name, :follower).where("name LIKE ?","%" + params[:search] + "%")
+      result += Cupon.where("title LIKE ?","%" + params[:search] + "%")
+      result
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-   root "mainpages#index"
+  root "mainpages#index", via: [:get,:post]
   # root "articles#index"
   resources :cupons
 end
