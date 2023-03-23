a4ea4d1fa9676a75a6397189cd2a6c5cc627eb1a
oscaregr <bavuey@hotmail.com>
2023-03-23 13:20:22 -0600

    Create user home.
    The user can see their coupons and the users they follow.
    The home page has a design, and can be accessed through the menu on the left.

--- app/controllers/mainpages_controller.rb
+++ app/controllers/mainpages_controller.rb
@@ -1,5 +1,23 @@
 class MainpagesController < ApplicationController
-    def index; 
-        @cupon = Cupon.all
+  def index
+    if params[:home]
+      if params[:cupon]
+        @result = user_cupons
+      elsif params[:following]
+        @result = user_following
+      end
+    else
+      @result = Cupon.all
+    end
+  end
+
+  private
+    def user_cupons
+      Cupon.all.where(user: current_user.id)
+    end
+
+    def user_following
+      follower = current_user.follower&.split(",")&.map(&:to_i)
+      User.all.where(id: follower)
     end
 end
