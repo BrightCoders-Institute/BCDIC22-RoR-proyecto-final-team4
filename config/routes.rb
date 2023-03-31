d1d2c8a5634c4502b35913f059e2ae23b30d0444
ever <ealcaraz12@ucol.mx>
2023-03-31 14:30:08 -0600

    indentation and unnecessary spaces fixed

--- config/routes.rb
+++ config/routes.rb
@@ -6,18 +6,13 @@ Rails.application.routes.draw do
 
   delete '/logout', to: 'sessions#destroy'
 
-
   # Defines the root path route ("/")
   root "mainpages#index", via: [:get,:post]
   resources :mainpages, only: [:update]
-
- 
   resources :cupons do
     member do
       post 'increment_punctuation', to: 'cupons#update'
       post 'decrement_punctuation', to: 'cupons#update'
     end
   end
-  
-  
 end
