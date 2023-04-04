82bd1b7a190dd9e4dd3465f0f8de86cd6f2838d1
DiegoCoronaBarragan <d.coronabarragan@gmail.com>
2023-04-03 19:12:37 -0600

    Fix seeds and add admin email As an admin, I want an admin email with which I can manage the categories so that is necessary to have a default admin email.

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,4 +1,5 @@
 class CategoriesController < ApplicationController
+  before_action :authenticate_user
   before_action :set_category, only: %i[ show edit update destroy ]
 
   # GET /categories or /categories.json
@@ -61,5 +62,11 @@ class CategoriesController < ApplicationController
     # Only allow a list of trusted parameters through.
     def category_params
       params.require(:category).permit(:namecategory)
+    end
+
+    def authenticate_user
+      if current_user.nil? || current_user.email != "procuponss@gmail.com"
+        redirect_to root_path
+      end
     end
 end
