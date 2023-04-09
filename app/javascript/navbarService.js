16b92bc9c5281cf80154f23a2b35ab369d306d97
DiegoCoronaBarragan <d.coronabarragan@gmail.com>
2023-04-08 23:48:14 -0600

    fix changes

--- app/javascript/navbarService.js
+++ app/javascript/navbarService.js
@@ -2,12 +2,10 @@ const openNav = () => {
   if (window.innerWidth >= 1080) {
     document.getElementById("mySidenavService").style.width = "12%";
     document.getElementById("mainService").style.marginLeft = "96%";
-    // document.getElementById("mainService").style.background = "none";
     document.getElementById("logoService").style.display = "none";
   }else {
     document.getElementById("mySidenavService").style.width = "100%";
     document.getElementById("mainService").style.marginLeft = "100%";
-    // document.getElementById("mainService").style.background = "none";
     document.getElementById("logoService").style.display = "none";
   }
 }
